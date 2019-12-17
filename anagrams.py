8a45f4e705a179994b7d3c35fb3029e4d2cde7a5
James Maupin <jmsMaupinRC@gmail.com>
2019-12-17 08:49:12 -0500

    Fix anagrams to pass time benchmarks

--- anagrams.py
+++ anagrams.py
@@ -8,7 +8,7 @@
     for an arbitrary list of strings.
 
 """
-__author__ = "???"
+__author__ = "jmsMaupin1"
 
 import sys
 
@@ -39,20 +39,24 @@ def find_anagrams(words):
         {'dgo': ['dog'], 'act': ['cat', 'act']}
 
     """
-    anagrams = {
-        alphabetize(word): [
-            w for w in words
-            if alphabetize(w) == alphabetize(word)]
-        for word in words}
+    anagrams = {}
+    for word in words:
+        alphabetized_word = alphabetize(word)
+        if alphabetized_word not in anagrams:
+            anagrams[alphabetized_word] = [word]
+        else:
+            anagrams[alphabetized_word] += [word]
+
     return anagrams
 
 
 if __name__ == "__main__":
     # run find anagrams of first argument
     if len(sys.argv) < 2:
-        print "Please specify a word file!"
+        print("Please specify a word file!")
         sys.exit(1)
     else:
         with open(sys.argv[1], 'r') as handle:
             words = handle.read().split()
-            print find_anagrams(words)
+            print(find_anagrams(words))
+
